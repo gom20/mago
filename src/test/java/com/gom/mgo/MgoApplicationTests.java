ea444c51a1d2c2d9bacddb77d60598ee40b90872
gom20 <rhaldud89@gmail.com>
2022-12-12 12:54:19 +0900

    commit

---
+++ src/test/java/com/gom/mgo/MgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.mgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
