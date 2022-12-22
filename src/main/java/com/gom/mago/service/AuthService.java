0eb99266f9f18bbc2ec7a2a90f940523a8d6123f
gom20 <rhaldud89@gmail.com>
2022-12-22 15:06:34 +0900

    Auth 서비스 생성

---
+++ src/main/java/com/gom/mago/service/AuthService.java
@@ -0,0 +1,37 @@
+package com.gom.mago.service;
+
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.gom.mago.dto.member.Login;
+import com.gom.mago.entity.Member;
+import com.gom.mago.jwt.JwtTokenProvider;
+import com.gom.mago.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+	   
+    @Transactional
+    public Login.Response login(Login.Request request) {
+    	// 1. 가입 여부 확인
+    	Member member = memberRepository.findByEmail(request.getEmail())
+    		  .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+      
+    	// 2. 비밀번호 확인
+    	if(passwordEncoder.matches(request.getPassword(), member.getPassword())) {
+    		return Login.Response.fromEntity(jwtTokenProvider.createToken(member.getUsername()));
+    	} else {
+    		throw new IllegalArgumentException("잘못된 패스워드 입니다.");
+    	}
+    	
+    }
+}
