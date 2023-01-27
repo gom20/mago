ff1d406ad7e13fa7bdc6137b33efa9ad1aa58132
gom20 <rhaldud89@gmail.com>
2023-01-27 15:58:53 +0900

    email error resolved

--- src/main/java/com/gom/mago/service/EmailService.java
+++ src/main/java/com/gom/mago/service/EmailService.java
@@ -77,7 +77,7 @@ public class EmailService {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(to);
 		message.setSubject("마운틴고 회원 가입 본인 인증");
-		String verifyEmailUrl = domain + "/api/auth/verifyEmail?token=" + token;
+		String verifyEmailUrl = "http://" + domain + "/api/auth/verifyEmail?token=" + token;
 		sendEmail(message, "verify_email", new HashMap<String, Object>(){{put("verifyEmailUrl", verifyEmailUrl);}});
 	}
 	
